2cac3ce481605395858c1e131e6f53e8bcbe55bc
Klev12 <kevinsibri2003@hotmail.com>
2023-11-17 10:53:40 -0500

    create proyect

---
+++ src/main/kotlin/com/zonav/Proyectopiensa/ProyectoPiensaApplication.kt
@@ -0,0 +1,11 @@
+package com.zonav.Proyectopiensa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyectoPiensaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyectoPiensaApplication>(*args)
+}
---
+++ src/test/kotlin/com/zonav/Proyectopiensa/ProyectoPiensaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zonav.Proyectopiensa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyectoPiensaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
